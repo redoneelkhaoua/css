d1b4de77151d2e29b419f9f1f4eeb700ed041131
redONE <elkhaouar@gmail.com>
2022-04-13 04:08:59 +0000

    tlaga

---
+++ reactatest/src/PosteCreator.js
@@ -0,0 +1,52 @@
+import React from 'react';
+function CreePost({ input , setInput ,posts,setposts }){
+
+
+    const onInputChange =(e)=>{
+        setInput(e.target.value);
+    };
+    
+   const onClickSubmit =(e)=>{
+         e.preventDefault();  
+ 
+         fetch('http://localhost:5052/api/Note/',{
+             method : 'post',
+             headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+                'Access-Control-Allow-Origin': '*'
+             },
+             body :JSON.stringify({
+                note:input ,
+                type :"note",
+                idCase:1
+             })
+         }).then(response=>{
+             console.log(response)
+             if(response.status==200){
+                 console.log("success")
+             }
+         })
+        setInput("");
+
+   }
+
+    return(
+   
+    <form onSubmit={onClickSubmit}  >
+ <div id='create'>
+        <input type="text" className='creeNote' 
+        placeholder='Saisissez une note...' 
+        onChange={onInputChange}
+        value={input}
+        />
+        
+        <div id="afficheNote">
+            <button type="submit" id="logoAffiche" />
+           
+        </div>
+        </div>
+    </form>
+    
+    );
+}
+export default CreePost ;
